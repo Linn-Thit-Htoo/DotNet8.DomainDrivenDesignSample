23a199d532f110964bece89e9eb4644f54dcc481
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-01 15:51:30 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Api/Features/Blog/BL_Blog.cs
+++ DotNet8.DomainDrivenDesignSample.Api/Features/Blog/BL_Blog.cs
@@ -3,58 +3,56 @@ using DotNet8.DomainDrivenDesignSample.Domain.Models.Blog;
 using DotNet8.DomainDrivenDesignSample.Domain.Resources;
 using DotNet8.DomainDrivenDesignSample.Domain.Shared;
 
-namespace DotNet8.DomainDrivenDesignSample.Api.Features.Blog
+namespace DotNet8.DomainDrivenDesignSample.Api.Features.Blog;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly IBlogRepository _blogRepository;
+
+    public BL_Blog(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public BL_Blog(IBlogRepository blogRepository)
+    public async Task<Result<BlogListResponseModel>> GetBlogList()
+    {
+        return await _blogRepository.GetBlogList();
+    }
+
+    public async Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel)
+    {
+        return await _blogRepository.CreateBlog(requestModel);
+    }
+
+    public async Task<Result<BlogResponseModel>> UpdateBlog(BlogRequestModel requestModel, int id)
+    {
+        Result<BlogResponseModel> responseModel;
+
+        if (id <= 0)
         {
-            _blogRepository = blogRepository;
+            responseModel = Result<BlogResponseModel>.FailureResult(MessageResource.InvalidId);
+            goto result;
         }
 
-        public async Task<Result<BlogListResponseModel>> GetBlogList()
-        {
-            return await _blogRepository.GetBlogList();
-        }
-
-        public async Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel)
-        {
-            return await _blogRepository.CreateBlog(requestModel);
-        }
-
-        public async Task<Result<BlogResponseModel>> UpdateBlog(BlogRequestModel requestModel, int id)
-        {
-            Result<BlogResponseModel> responseModel;
-
-            if (id <= 0)
-            {
-                responseModel = Result<BlogResponseModel>.FailureResult(MessageResource.InvalidId);
-                goto result;
-            }
-
-
-            responseModel = await _blogRepository.UpdateBlog(requestModel, id);
+        responseModel = await _blogRepository.UpdateBlog(requestModel, id);
 
         result:
-            return responseModel;
+        return responseModel;
+    }
+
+    public async Task<Result<BlogResponseModel>> DeleteBlog(int id)
+    {
+        Result<BlogResponseModel> responseModel;
+
+        if (id <= 0)
+        {
+            responseModel = Result<BlogResponseModel>.FailureResult(MessageResource.InvalidId);
+            goto result;
         }
 
-        public async Task<Result<BlogResponseModel>> DeleteBlog(int id)
-        {
-            Result<BlogResponseModel> responseModel;
-
-            if (id <= 0)
-            {
-                responseModel = Result<BlogResponseModel>.FailureResult(MessageResource.InvalidId);
-                goto result;
-            }
-
-            responseModel = await _blogRepository.DeleteBlog(id);
+        responseModel = await _blogRepository.DeleteBlog(id);
 
         result:
-            return responseModel;
-        }
+        return responseModel;
     }
 }
