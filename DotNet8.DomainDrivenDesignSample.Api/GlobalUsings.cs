c4d68027bea6f5a2d186155af8c731ca0930cd4a
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-01 15:52:34 +0630

    [Added] Global Usings

---
+++ DotNet8.DomainDrivenDesignSample.Api/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using DotNet8.DomainDrivenDesignSample.Api.Features.Blog;
+global using DotNet8.DomainDrivenDesignSample.Domain.Interfaces.Blog;
+global using DotNet8.DomainDrivenDesignSample.Infrastructure.AppDbContexts;
+global using DotNet8.DomainDrivenDesignSample.Infrastructure.Repositories;
+global using Microsoft.EntityFrameworkCore;
