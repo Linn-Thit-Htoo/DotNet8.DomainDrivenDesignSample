ec1b6b82f9282e3c042ff217b4aabc5a45a5ea36
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-01 15:54:55 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Domain/Shared/DevCode.cs
+++ DotNet8.DomainDrivenDesignSample.Domain/Shared/DevCode.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.DomainDrivenDesignSample.Domain.Shared;
 
-namespace DotNet8.DomainDrivenDesignSample.Domain.Shared
+public static class DevCode
 {
-    public static class DevCode
+    public static bool IsNullOrEmpty(this string str)
     {
-        public static bool IsNullOrEmpty(this string str)
-        {
-            return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-        }
+        return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
     }
 }
