cab4fb5c1267d9fe6f28a4c36de59d90983fbafe
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-01 15:52:58 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Api/Features/Blog/BlogController.cs
+++ DotNet8.DomainDrivenDesignSample.Api/Features/Blog/BlogController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace DotNet8.DomainDrivenDesignSample.Api.Features.Blog;
+﻿namespace DotNet8.DomainDrivenDesignSample.Api.Features.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
--- DotNet8.DomainDrivenDesignSample.Api/GlobalUsings.cs
+++ DotNet8.DomainDrivenDesignSample.Api/GlobalUsings.cs
@@ -7,4 +7,5 @@ global using DotNet8.DomainDrivenDesignSample.Domain.Resources;
 global using DotNet8.DomainDrivenDesignSample.Domain.Shared;
 global using DotNet8.DomainDrivenDesignSample.Infrastructure.AppDbContexts;
 global using DotNet8.DomainDrivenDesignSample.Infrastructure.Repositories;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
