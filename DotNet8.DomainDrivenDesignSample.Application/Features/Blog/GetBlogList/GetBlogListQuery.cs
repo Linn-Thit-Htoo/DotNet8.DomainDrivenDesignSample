c6b6dd0bb06f1d02770db61be2b85fdd480ae40a
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:38:55 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.DomainDrivenDesignSample.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,15 +1,7 @@
 ﻿using DotNet8.DomainDrivenDesignSample.Domain.Features.Blog;
 using DotNet8.DomainDrivenDesignSample.Domain.Shared;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.GetBlogList
-{
-    public class GetBlogListQuery : IRequest<Result<BlogListResponseModel>>
-    {
-    }
-}
+namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQuery : IRequest<Result<BlogListResponseModel>> { }
--- DotNet8.DomainDrivenDesignSample.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.DomainDrivenDesignSample.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,26 +1,20 @@
 ﻿using DotNet8.DomainDrivenDesignSample.Domain.Features.Blog;
 using DotNet8.DomainDrivenDesignSample.Domain.Shared;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.GetBlogList
+namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListResponseModel>>
 {
-    public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListResponseModel>>
+    private readonly IBlogRepository _blogRepository;
+
+    public GetBlogListQueryHandler(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public GetBlogListQueryHandler(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
-
-        public async Task<Result<BlogListResponseModel>> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
-        {
-            return await _blogRepository.GetBlogList();
-        }
+    public async Task<Result<BlogListResponseModel>> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+    {
+        return await _blogRepository.GetBlogList();
     }
 }
