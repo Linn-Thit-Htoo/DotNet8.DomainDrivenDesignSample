e26781bb87b34ec40e8c82d2bc56e5c4a2818f42
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:39:01 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.DomainDrivenDesignSample.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -4,7 +4,8 @@ using MediatR;
 
 namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.GetBlogList;
 
-public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListResponseModel>>
+public class GetBlogListQueryHandler
+    : IRequestHandler<GetBlogListQuery, Result<BlogListResponseModel>>
 {
     private readonly IBlogRepository _blogRepository;
 
@@ -13,7 +14,10 @@ public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<
         _blogRepository = blogRepository;
     }
 
-    public async Task<Result<BlogListResponseModel>> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+    public async Task<Result<BlogListResponseModel>> Handle(
+        GetBlogListQuery request,
+        CancellationToken cancellationToken
+    )
     {
         return await _blogRepository.GetBlogList();
     }
