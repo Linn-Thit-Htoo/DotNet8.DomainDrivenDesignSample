0299edae17323471b8dab35a2ea417e9ce45cf68
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:40:41 +0630

    [Added] Global Usings in Application

---
+++ DotNet8.DomainDrivenDesignSample.Application/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8.DomainDrivenDesignSample.Domain.Features.Blog;
+global using DotNet8.DomainDrivenDesignSample.Domain.Shared;
+global using MediatR;
