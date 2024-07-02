14b59bc3881214e745808ad98a56a60f473f2e50
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:41:20 +0630

    Format  Code

--- DotNet8.DomainDrivenDesignSample.Infrastructure/Db/AppDbContext.cs
+++ DotNet8.DomainDrivenDesignSample.Infrastructure/Db/AppDbContext.cs
@@ -1,18 +1,11 @@
 ﻿using DotNet8.DomainDrivenDesignSample.Domain.Features.Blog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.DomainDrivenDesignSample.Infrastructure.Db
+namespace DotNet8.DomainDrivenDesignSample.Infrastructure.Db;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
-        public DbSet<TblBlog> TblBlogs { get; set; }
-    }
+    public DbSet<TblBlog> TblBlogs { get; set; }
 }
