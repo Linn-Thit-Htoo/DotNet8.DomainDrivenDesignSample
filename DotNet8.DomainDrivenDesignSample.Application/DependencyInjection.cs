e009e90707b1515c6d1727b72f8565e7272bb8ac
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:39:56 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Application/DependencyInjection.cs
+++ DotNet8.DomainDrivenDesignSample.Application/DependencyInjection.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.DomainDrivenDesignSample.Application
+namespace DotNet8.DomainDrivenDesignSample.Application;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddMediatR(this IServiceCollection services)
     {
-        public static IServiceCollection AddMediatR(this IServiceCollection services)
-        {
-            services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-            return services;
-        }
+        services.AddMediatR(cf =>
+            cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)
+        );
+        return services;
     }
 }
