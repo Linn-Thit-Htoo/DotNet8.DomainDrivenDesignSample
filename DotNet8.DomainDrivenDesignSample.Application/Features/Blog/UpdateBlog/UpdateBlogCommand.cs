61c579fefc05e3be47edaa1c031a243162a1ba6d
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:39:17 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.DomainDrivenDesignSample.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,24 +1,18 @@
 ﻿using DotNet8.DomainDrivenDesignSample.Domain.Features.Blog;
 using DotNet8.DomainDrivenDesignSample.Domain.Shared;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.UpdateBlog
+namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.UpdateBlog;
+
+public class UpdateBlogCommand : IRequest<Result<BlogResponseModel>>
 {
-    public class UpdateBlogCommand : IRequest<Result<BlogResponseModel>>
+    public BlogRequestModel RequestModel;
+
+    public int BlogId;
+
+    public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
     {
-        public BlogRequestModel RequestModel;
-
-        public int BlogId;
-
-        public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
-        {
-            this.RequestModel = requestModel;
-            BlogId = blogId;
-        }
+        this.RequestModel = requestModel;
+        BlogId = blogId;
     }
 }
