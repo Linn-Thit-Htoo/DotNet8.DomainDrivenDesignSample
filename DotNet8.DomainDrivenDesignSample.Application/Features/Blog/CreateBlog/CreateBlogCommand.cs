d7fb95d45485a9a5cdf62be2d803774127d36752
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-02 11:37:56 +0630

    Format Code

--- DotNet8.DomainDrivenDesignSample.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
+++ DotNet8.DomainDrivenDesignSample.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
@@ -1,21 +1,15 @@
 ﻿using DotNet8.DomainDrivenDesignSample.Domain.Features.Blog;
 using DotNet8.DomainDrivenDesignSample.Domain.Shared;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.CreateBlog
+namespace DotNet8.DomainDrivenDesignSample.Application.Features.Blog.CreateBlog;
+
+public class CreateBlogCommand : IRequest<Result<BlogResponseModel>>
 {
-    public class CreateBlogCommand : IRequest<Result<BlogResponseModel>>
-    {
-        public BlogRequestModel RequestModel;
+    public BlogRequestModel RequestModel;
 
-        public CreateBlogCommand(BlogRequestModel requestModel)
-        {
-            this.RequestModel = requestModel;
-        }
+    public CreateBlogCommand(BlogRequestModel requestModel)
+    {
+        this.RequestModel = requestModel;
     }
 }
